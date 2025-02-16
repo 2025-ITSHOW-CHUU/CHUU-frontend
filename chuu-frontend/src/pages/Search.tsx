cbb420a294e603dec08fa10e2bbc65f7057aa8b8
ahkjeflhkeuvha <w2317@e-mirim.hs.kr>
2025-02-16 13:06:17 +0900

    Search Function

--- chuu-frontend/src/pages/Search.tsx
+++ chuu-frontend/src/pages/Search.tsx
@@ -1,16 +1,46 @@
-import React from "react";
-import { useEffect, useState } from "react";
+import React, { useEffect, useState } from "react";
 import SearchBar from "../components/SearchBar.tsx";
 import def from "../styles/Default.module.css";
 import style from "../styles/Search.module.css";
 import { ReactComponent as Logo } from "../assets/logo.svg";
 import { IoIosArrowBack } from "react-icons/io";
+import teacherData from "../assets/teachers.json";
+
+type TeacherType = {
+  name: string;
+  photo: string;
+  personality: string;
+  hashtags: string[];
+};
 
 function Search({}) {
-  const [search, setSearch] = useState("");
+  const [search, setSearch] = useState<string>("");
+  const [teachers, setTeachers] = useState<TeacherType[]>([]);
+  const [originalTeachers, setOriginalTeachers] = useState<TeacherType[]>([]);
+
+  useEffect(() => {
+    setTeachers(teacherData.teachers);
+    setOriginalTeachers(teacherData.teachers);
+  }, []);
 
   const onChange = (value: string) => {
     setSearch(value);
+    searchResult(value);
+  };
+
+  const searchResult = (value: string) => {
+    if (value === "") {
+      setTeachers(originalTeachers);
+      return;
+    }
+
+    const searchedResult = originalTeachers.filter(
+      (teacher: TeacherType) =>
+        teacher.name.includes(value) ||
+        teacher.hashtags.some((hashtag) => hashtag.includes(value))
+    );
+
+    setTeachers(searchedResult);
   };
 
   return (
@@ -21,6 +51,19 @@ function Search({}) {
       <div className={`${style["SearchDiv"]}`}>
         <IoIosArrowBack size="25px" />
         <SearchBar value={search} onChange={onChange} />
+      </div>
+      <div className={`${style["SearchResult"]}`}>
+        {(teachers as TeacherType[]).map((teacher) => (
+          <div>
+            <div>
+              <img src={teacher.photo} />
+            </div>
+            <div>
+              <p>{teacher.name}</p>
+              <p>{teacher.personality}</p>
+            </div>
+          </div>
+        ))}
       </div>
     </div>
   );
