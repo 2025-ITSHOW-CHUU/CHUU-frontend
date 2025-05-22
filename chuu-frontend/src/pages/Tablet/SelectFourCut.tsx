90fa9837bbefad472f1aba6fead724a436cb0ad5
ahkjeflhkeuvha <w2317@e-mirim.hs.kr>
2025-05-22 18:54:47 +0900

    프레임 선택 css 추가

--- chuu-frontend/src/pages/Tablet/SelectFourCut.tsx
+++ chuu-frontend/src/pages/Tablet/SelectFourCut.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import filterJson from "../../assets/filter.json";
 import { useNavigate } from "react-router-dom";
+import style from "../../styles/SelectFourCut.module.css";
 
 function SelectFourCut() {
   const navigate = useNavigate();
@@ -13,11 +14,22 @@ function SelectFourCut() {
     });
   };
   return (
-    <div>
+    <div className={style.selectFrameContainer}>
       <h1>프레임을 선택하세요!</h1>
-      {filterJson.map((filter) => {
-        return <div onClick={() => handleClick(filter)}>{filter.title}</div>;
-      })}
+      <div className={style.framesContainer}>
+        {filterJson.map((filter) => {
+          return (
+            <div className={style.frameContainer}>
+              <img
+                onClick={() => handleClick(filter)}
+                src={filter.finalFrame}
+                alt={filter.title}
+              />
+              <p>{filter.title}</p>
+            </div>
+          );
+        })}
+      </div>
     </div>
   );
 }
