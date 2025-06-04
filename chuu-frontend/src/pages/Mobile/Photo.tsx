e5f84195d9b19d3df0401e5b7cb8228d62f54bf3
ahkjeflhkeuvha <w2317@e-mirim.hs.kr>
2025-06-04 12:09:48 +0900

    printFile 함수 수정

--- chuu-frontend/src/pages/Mobile/Photo.tsx
+++ chuu-frontend/src/pages/Mobile/Photo.tsx
@@ -121,19 +121,24 @@ function PhotoBooth() {
 
   const printFile = async (base64Url: string) => {
     try {
+      const base64Data = base64Url.split(",")[1];
+
       const response = await fetch("http://localhost:3000/upload", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
         },
-        body: JSON.stringify({ image: base64Url.split(",")[1] }),
+        body: JSON.stringify({ file: base64Data }),
       });
 
-      const data = await response.json();
+      const html = await response.text();
 
-      if (data.filename) {
-        const printUrl = `http://localhost:3000/print/${data.filename}`;
-        window.open(printUrl, "_blank");
+      const printWindow = window.open("", "_blank");
+      if (printWindow) {
+        printWindow.document.write(html);
+        printWindow.document.close();
+      } else {
+        alert("팝업 차단을 해제해주세요!");
       }
     } catch (err) {
       console.error("프린트 에러 발생:", err);
