d381bf010a7cc3185a23cb8519a7dad02d4f6274
subin1848 <s2315@e-mirim.hs.kr>
2025-06-13 10:10:19 +0900

    ✨ [feat] Home 페이지에 Widget 추가

--- chuu-frontend/src/pages/Mobile/Home.tsx
+++ chuu-frontend/src/pages/Mobile/Home.tsx
@@ -1,10 +1,12 @@
 import React, { useState } from "react";
 import SearchBar from "../../components/SearchBar";
+import Footer from "../../components/Footer";
 import EncateResult from "../../components/EncateResult";
 import def from "../../styles/Default.module.css";
 import style from "../../styles/Home.module.css";
 import { ReactComponent as Logo } from "../../assets/logo.svg";
 import { ReactComponent as MainCharacter } from "../../assets/main_character.svg";
+import { ReactComponent as Widget } from "../../assets/widget.svg";
 import { IoIosArrowForward } from "react-icons/io";
 import { useNavigate } from "react-router-dom";
 
@@ -12,13 +14,18 @@ function Home() {
   const [search, setSearch] = useState("");
   const navigate = useNavigate();
 
-  const onChange = (value: string) => {
+  const onChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const value = e.target.value;
     setSearch(value);
     console.log(value);
   };
 
   const onClick = (dir: string) => () => {
     navigate(dir);
+  };
+
+  const handleChatWidgetClick = async () => {
+    navigate("/chatbot");
   };
 
   return (
@@ -44,6 +51,22 @@ function Home() {
         <IoIosArrowForward />
       </button>
       <EncateResult />
+      {/* 채팅 위젯 */}
+      <div className={style["chatWidgetContainer"]}>
+        <div className={style["chatBubbleWrapper"]}>
+          <div className={style["chatBubble"]}>
+            선생님 AI와 대화해 보세요!
+            <div className={style["chatTail"]}></div>
+          </div>
+          <div 
+            className={`${style["chatIcon"]}`} 
+            onClick={handleChatWidgetClick}
+          >
+            <Widget/>
+          </div>
+        </div>
+      </div>
+      <Footer />
     </div>
   );
 }
