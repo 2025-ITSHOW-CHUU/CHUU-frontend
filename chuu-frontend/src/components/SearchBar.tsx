c3d0cd74a71576698c988827943cc373f3ee9f59
ahkjeflhkeuvha <w2317@e-mirim.hs.kr>
2025-06-14 16:47:14 +0900

    input 수정

--- chuu-frontend/src/components/SearchBar.tsx
+++ chuu-frontend/src/components/SearchBar.tsx
@@ -1,21 +1,36 @@
 import React from "react";
-import { IoSearchOutline } from "react-icons/io5";
 import style from "../styles/SearchBar.module.css";
+import { IoSearchOutline } from "react-icons/io5";
 
 function SearchBar(props: {
   value: string;
   onChange: (value: string) => void;
 }) {
-  const onChange = (e: React.ChangeEvent<HTMLElement>) => {
-    const target = e.target as HTMLButtonElement;
-    props.onChange(target.value);
+  const onChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    props.onChange(e.target.value);
   };
 
+  const isActive = props.value.length > 0;
+
   return (
-    <div className={`${style["SearchBar"]}`}>
-      <IoSearchOutline />
+    <div className={style.SearchContainer}>
+      <IoSearchOutline
+        style={{
+          position: "absolute",
+          top: "50%",
+          left: "10px",
+          transform: "translateY(-50%)",
+          color: "#d1d2d1",
+        }}
+      />
+      <label
+        className={`${style.FloatingLabel} ${isActive ? style.active : ""}`}
+      >
+        나의 추구미 선생님을 검색해 보세요!
+      </label>
       <input
-        className={`${style["SearchInput"]}`}
+        type="text"
+        className={style.SearchInput}
         value={props.value}
         onChange={onChange}
       />
--- chuu-frontend/src/pages/Web/Main.tsx
+++ chuu-frontend/src/pages/Web/Main.tsx
@@ -33,7 +33,9 @@ function Main() {
   useEffect(() => {
     const getPosts = async () => {
       try {
-        const response = await axios.get("/post");
+        const response = await axios.get(
+          "https://chuu.mirim-it-show.site/post"
+        );
         setPosts(response.data.data);
         // console.log(response.data.data);
       } catch (error) {
